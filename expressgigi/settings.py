5050ee7867a1ed06550875a1cb951e898747aa7d
NephygTdkingz <tdkingzict@gmail.com>
2025-02-12 19:00:16 +0100

    added render to host

--- expressgigi/settings.py
+++ expressgigi/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh", 'localhost', 'gigifastexpress.com', 'www.gigifastexpress.com']
+ALLOWED_HOSTS = ["127.0.0.1", ".onrender.com", 'localhost', 'gigifastexpress.com', 'www.gigifastexpress.com']
 
 
 # Application definition
